2678f5d0c2909ee9d4038975bd01a18d9dd8326e
edwardfclark <edwardfclark@gmail.com>
2018-09-01 17:03:32 +0100

    Made a lot of changes!
    Implemented a lot of the core logic of the program, mostly working on
    App.js. I also added/changed some of the IDs on components to correspond
    to the requirements of the project on freeCodeCamp.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import SessionButtons from "./SessionButtons.js";
 import BreakButtons from "./BreakButtons.js";
 
+let countdownTimer = null;
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sessionState: "paused",
+      sessionState: "session",
       breakLength: 5,
       sessionLength: 25,
       mins: 25,
-      secs: 0
+      secs: 0,
+      timer: null
     }
 
     this.incBreak = this.incBreak.bind(this);
@@ -19,7 +22,11 @@ class App extends Component {
     this.incSession = this.incSession.bind(this);
     this.decSession = this.decSession.bind(this);
     this.getTimer = this.getTimer.bind(this);
+    this.countdown = this.countdown.bind(this);
+    this.startCountdown = this.startCountdown.bind(this);
   }
+
+  
 
   //Simple methods to increment or decrement the breakLength or sessionLength.
   //These will be passed to SessionButtons and BreakButtons as props.
@@ -73,15 +80,44 @@ class App extends Component {
     }
   }
 
+  //This method is the callback method for the setInterval call.
+  //It counts down from the currently displayed value in seconds.
+  //Because this is a React program, all of the DOM manipulation is handled by re-rendering the app when state is changed.
+  countdown() {
+    //This line runs once per call of countdown() and decrements secs by 1 every time.
+    //If secs is zero, it instead sets secs to 59 and decrements mins by 1.
+    this.state.secs > 0 ? this.setState({ secs: this.state.secs - 1 }) : this.setState({secs: 59, mins: this.state.mins - 1});
+
+    //When the timer reaches 0:00, swap the sessionState and set mins to either breakLength or sessionLength, depending on what state we're swapping to.
+    if (this.state.secs === 0 && this.state.mins === 0) {
+      this.state.sessionState == "session" ? this.setState({sessionState: "break", mins: this.state.breakLength}) : this.setState({sessionState: "session", mins: this.state.sessionLength}); 
+    }
+  }
+  
+  
+
+  startCountdown() {
+    if (countdownTimer == null) {
+      countdownTimer = setInterval(this.countdown, 1000);
+    } else {
+      clearInterval(countdownTimer);
+      countdownTimer = null;
+    }
+  }
+
   render() {
+    
     return (
       <div className="clock-container">
-        <h2 id="session-label">Session ID</h2>
-        <h1 id="timer">{this.getTimer()}</h1>
+        <h2 id="timer-label">Session ID</h2>
+        <h1 id="time-left">{this.getTimer()}</h1>
+        <h3 id="start_stop" onClick={this.startCountdown}>Start/Stop</h3>
+        <h3 id="reset">Reset</h3>
         <div className="buttons">
           <SessionButtons className="session-buttons" sessionLength={this.state.sessionLength} incSession={this.incSession} decSession={this.decSession} />
           <BreakButtons className="break-buttons" breakLength={this.state.breakLength} incBreak={this.incBreak} decBreak={this.decBreak} />
         </div>
+        
       </div>
     );
   }
--- src/BreakButtons.js
+++ src/BreakButtons.js
@@ -3,8 +3,8 @@ import React from 'react';
 function BreakButtons(props) {
     return (
         <div>
-            <h3>Break</h3>
-            <h4><span onClick={props.decBreak}>-</span> {props.breakLength} <span onClick={props.incBreak}>+</span></h4>
+            <h3 id="break-label">Break Length</h3>
+            <h4><span id="break-decrement" onClick={props.decBreak}>-</span> <span id="break-length">{props.breakLength}</span> <span id="break-increment" onClick={props.incBreak}>+</span></h4>
         </div>
     );
 }
